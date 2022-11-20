fb101d737516ef3a30d7c574b79ef8a1f8039f66
Zenthae <32647899+Zenthae@users.noreply.github.com>
2022-11-20 23:33:48 +0100

    clean

--- packages/client/src/main.rs
+++ packages/client/src/main.rs
@@ -3,11 +3,7 @@
     windows_subsystem = "windows"
 )]
 
-use shared::database::Database;
-
 fn main() {
-    let database = Database::new();
-
     tauri::Builder::default()
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
