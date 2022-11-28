225068fd2d8691128010503d4d75c9b45a314bf3
Zenthae <32647899+Zenthae@users.noreply.github.com>
2022-11-28 15:10:17 +0100

    event loop

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,11 @@
     windows_subsystem = "windows"
 )]
 
+use std::sync::{Arc, Mutex};
+
 use recorder::Recorder;
 use sea_orm::DatabaseConnection;
+use tauri::WindowEvent;
 
 mod command;
 mod database;
@@ -22,16 +25,29 @@ async fn main() {
     let db = database::get_connection().await;
     database::run_migrations(&db).await;
 
-    let mut recorder = Recorder::new();
+    let recorder = Arc::new(Mutex::new(Recorder::new()));
 
-    recorder.init();
+    recorder.lock().unwrap().init();
 
-    recorder.run();
+    recorder.lock().unwrap().run();
 
     let state = AppState { database: db };
 
     tauri::Builder::default()
         .manage(state)
+        .on_window_event(move |event| {
+            if let WindowEvent::Focused(is_focused) = event.event() {
+                if *is_focused {
+                    recorder.lock().unwrap().stop();
+                } else {
+                    recorder.lock().unwrap().run();
+                }
+            };
+
+            if let WindowEvent::CloseRequested { .. } = event.event() {
+                recorder.lock().unwrap().stop();
+            }
+        })
         .invoke_handler(tauri::generate_handler![command::get_event_by_id])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
